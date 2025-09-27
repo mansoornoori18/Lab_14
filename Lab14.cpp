49c9fc95b1f621406ee456c552c6af13716ff219
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-09-26 22:05:00 -0700

    update

--- Lab14.cpp
+++ Lab14.cpp
@@ -1,7 +1,20 @@
-#include<iostream>
+#include<iostream>  // COMSC-210 | Lab 14 | Mansoor Noori
+#include<iomanip>   // IDE Visual Studio,
 
+using namespace std; // to void using std over and over
 
-using namespace std;
+// Color class definition
+class Color {
+    private:
+    // Private member variables to store RGB values
+    int red;
+    int green;
+    int blue;
+
+    public:
+
+}
+
 
 int main (){
 
@@ -9,5 +22,5 @@ int main (){
 
 
     return 0;
-    
+
 }
